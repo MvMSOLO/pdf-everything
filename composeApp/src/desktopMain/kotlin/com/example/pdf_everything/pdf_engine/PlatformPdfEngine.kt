ba6b7bfb89f63e73647acef736ed6a8dfd461573
MvMSOLO <mirzayevavazbek569@gmail.com>
2026-09-23 19:57:23 +0500

    fix(platform): align expect actual package declarations

--- composeApp/src/desktopMain/kotlin/com/example/pdf_everything/pdf_engine/PlatformPdfEngine.kt
+++ composeApp/src/desktopMain/kotlin/com/example/pdf_everything/pdf_engine/PlatformPdfEngine.kt
@@ -1,5 +1,5 @@
-package com.example.pdf_everything.pdf_engine
+package com.example.pdf_everything.pdf_engine.api
 
-import com.example.pdf_everything.pdf_engine.api.PdfEngine
+import com.example.pdf_everything.pdf_engine.DesktopPdfEngine
 
 actual fun createPdfEngine(): PdfEngine = DesktopPdfEngine()
